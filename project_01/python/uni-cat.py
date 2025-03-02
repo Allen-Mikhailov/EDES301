89d01dadfa16a67db9692a4495cfcd4529572c5c
Allen mikhailov <slinkyshelf8@gmail.com>
2025-03-02 15:36:10 -0600

    Project Start

---
+++ project_01/python/uni-cat.py
@@ -0,0 +1,102 @@
+"""
+--------------------------------------------------------------------------
+Uni-Cat
+--------------------------------------------------------------------------
+License:   
+Copyright 2025 Allen Mikhailov
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this 
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+
+Description
+
+Requirements:
+  -
+
+Uses:
+  -
+
+"""
+import time
+
+# import ht16k33 as HT16K33
+import drivers.button as BUTTON
+
+
+# ------------------------------------------------------------------------
+# Constants
+# ------------------------------------------------------------------------
+
+# None
+
+# ------------------------------------------------------------------------
+# Global variables
+# ------------------------------------------------------------------------
+
+# None
+
+# ------------------------------------------------------------------------
+# Functions / Classes
+# ------------------------------------------------------------------------
+
+class UniCat():
+    services = {}
+    drivers = {}
+
+    def __init__(self):
+        pass
+
+    def add_service(self, service):
+        self.services[service.name] = service
+
+    def add_driver(self, name, driver):
+        self.drivers[name] = driver
+
+
+
+
+# ------------------------------------------------------------------------
+# Main script
+# ------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    print("Program Start")
+
+    # Create instantiation of the people counter
+    
+    uni_cat = UniCat()
+
+    try:
+        # Run the people counter
+        uni_cat.run()
+
+    except KeyboardInterrupt:
+        # Clean up hardware when exiting
+        uni_cat.cleanup()
+
+    print("Program Complete")
+
