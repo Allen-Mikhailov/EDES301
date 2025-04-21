21fffe44cd1771846d3aebf22426d56b5027bb55
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2025-04-21 17:38:10 -0500

    Motor Driver working

--- project_01/python/configure_pins.sh
+++ project_01/python/configure_pins.sh
@@ -7,9 +7,9 @@ config-pin P1_26 i2c
 config-pin P1_28 i2c
 
 # Button pins
-config-pin P1_02 gpio
-config-pin P1_04 gpio
-config-pin P1_06 gpio
-config-pin P1_08 gpio
-config-pin P1_10 gpio
-config-pin P1_12 gpio
+config-pin P2_27 gpio
+config-pin P2_28 gpio
+config-pin P2_29 gpio
+config-pin P2_30 gpio
+config-pin P2_32 gpio
+config-pin P2_34 gpio
---
+++ project_01/python/drivers/L293DNE/L293DNE.py
@@ -0,0 +1,40 @@
+import Adafruit_BBIO.GPIO as GPIO
+
+class L293DNE:
+    m1_left: str
+    m1_right: str
+    m2_left: str
+    m2_right: str
+
+    def __init__(self, m1_left: str, m1_right: str, m2_left: str, m2_right: str):
+        self.m1_left  = m1_left
+        self.m1_right = m1_right
+        self.m2_left  = m2_left
+        self.m2_right = m2_right
+
+        GPIO.setup(m1_left, GPIO.OUT)
+        GPIO.setup(m1_right, GPIO.OUT)
+        GPIO.setup(m2_left, GPIO.OUT)
+        GPIO.setup(m2_right, GPIO.OUT)
+
+    def reset(self):
+        GPIO.output(self.m1_left, GPIO.LOW)
+        GPIO.output(self.m1_right, GPIO.LOW)
+        GPIO.output(self.m2_left, GPIO.LOW)
+        GPIO.output(self.m2_right, GPIO.LOW)
+        
+    def move_m1(self, direction: int):
+        left: int = 0:lua require("nvchad.reload").reload_theme()
+        right: int = 0
+
+        if direction == 1:
+            right = 1
+        elif direction == -1:
+            left = 1
+
+        GPIO.output(self.m1_left, left)
+        GPIO.output(self.m1_right, right)
+    
+if __name__ == "__main__":
+    driver = L293DNE("P2_29", "P2_31", "P2_33", "P2_35")
+    driver.move_m1(1)
